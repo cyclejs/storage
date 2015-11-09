9831ccacd2df278ec49085070c36f101c56699bb
Kahlil Lechelt <hello@kahlil.info>
2015-11-09 17:48:59 +0100

    use eslint-config-cycle

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import doAction from './lib/doAction';
-import responseCollection from './lib/responseCollection';
+import doAction from './lib/doAction'
+import responseCollection from './lib/responseCollection'
 
 export default function storageDriver(request$) {
   // Execute writing actions.
-  request$.subscribe((req) => doAction(req.target, req.action, req.key, req.value));
+  request$.subscribe((req) => {
+    doAction(req.target, req.action, req.key, req.value)
+  })
 
   // Return reading functions.
-  return responseCollection;
+  return responseCollection
 }
