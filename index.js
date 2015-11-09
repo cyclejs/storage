f3024b4fbda434ae4c54bdffecb230b214ecdcb0
Kahlil Lechelt <hello@kahlil.info>
2015-11-09 16:11:14 +0100

    add driver code

---
+++ index.js
@@ -0,0 +1,10 @@
+import doAction from './lib/doAction';
+import responseCollection from './lib/responseCollection';
+
+export default function storageDriver(request$) {
+  // Execute writing actions.
+  request$.subscribe((req) => doAction(req.target, req.action, req.key, req.value));
+
+  // Return reading functions.
+  return responseCollection;
+}
