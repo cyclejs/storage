ba464e865eecdb6f375a865164ae29c03d4baca7
Kahlil Lechelt <hello@kahlil.info>
2015-11-13 23:14:44 +0100

    does not need to be a constructor function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import storageDriver from './storageDriver'
+import writeToStore from './writeToStore'
+import responseCollection from './responseCollection'
 
-export function makeStorageDriver() {
-  return storageDriver
+export default function storageDriver(request$) {
+  // Execute writing actions.
+  request$.subscribe((request) => writeToStore(request))
+
+  // Return reading functions.
+  return responseCollection
 }
