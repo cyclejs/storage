e94276485ba2789b7547bb850c1f6155cc305363
Kahlil Lechelt <hello@kahlil.info>
2015-11-23 09:17:43 +0100

    add examples and refactor responseCollection

---
+++ example/app.js
@@ -0,0 +1,33 @@
+var h = CycleDOM.h
+var makeDOMDriver = CycleDOM.makeDOMDriver
+var storageDriver = CycleStorageDriver.default
+
+function main(response) {
+    const DOM = response.DOM
+    const storage = response.storage
+   const storageRequest$ = DOM.select('input')
+    .events('keydown')
+    .debounce(50)
+    .map(function(ev) {
+      return {
+        key: 'inputText',
+        value: ev.target.value
+      };
+    });
+
+  return {
+    DOM: storage.local.getItem('inputText')
+    .map(function(text) {
+      return h('div', [
+        h('input', { type: 'text', value: text}),
+        h('span', text),
+      ])
+    }),
+    storage: storageRequest$
+  };
+}
+
+Cycle.run(main, {
+  DOM: makeDOMDriver('#app'),
+  storage: storageDriver,
+})
--- src/responseCollection.js
+++ src/responseCollection.js
@@ -1,26 +1,40 @@
 import Rx from 'rx'
 
-export default {
-  // For localStorage.
-  local: {
-    key(n) {
+export default function(request$) {
+  return {
+    // For localStorage.
+    local: {
+      key(n) {
+        // Function returning Observable of the nth key.
+        // return Rx.Observable.just(localStorage.key(n))
+        return request$
+          .filter((request) => request.target === `local`)
+          .filter((request) => request.key === localStorage.key(n))
+          .distinctUntilChanged()
+          .startWith(localStorage.key(n))
+      },
+      // Function returning Observable of values.
+      getItem(key) {
+        // return Rx.Observable.just(localStorage.getItem(key))
+        const startWith = localStorage.getItem(key) || ``
+
+        return request$
+          .filter((request) => request.target === `local`)
+          .filter((request) => request.key === key)
+          .map((request) => request.value)
+          .startWith(startWith)
+      },
+    },
+    // For sessionStorage.
+    session: {
       // Function returning Observable of the nth key.
-      return Rx.Observable.just(localStorage.key(n))
+      key(n) {
+        return Rx.Observable.just(sessionStorage.key(n))
+      },
+      // Function returning Observable of values.
+      getItem(key) {
+        return Rx.Observable.just(sessionStorage.getItem(key))
+      },
     },
-    // Function returning Observable of values.
-    getItem(key) {
-      return Rx.Observable.just(localStorage.getItem(key))
-    },
-  },
-  // For sessionStorage.
-  session: {
-    // Function returning Observable of the nth key.
-    key(n) {
-      return Rx.Observable.just(sessionStorage.key(n))
-    },
-    // Function returning Observable of values.
-    getItem(key) {
-      return Rx.Observable.just(sessionStorage.getItem(key))
-    },
-  },
+  }
 }
