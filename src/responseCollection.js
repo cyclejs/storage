dc77a0a67c13cee4766165195788a0b079f662f5
Kahlil Lechelt <hello@kahlil.info>
2015-12-03 11:39:21 +0100

    refactor responseCollection

--- src/responseCollection.js
+++ src/responseCollection.js
@@ -1,43 +1,14 @@
-export default function(request$) {
-  const local$ = request$.filter((req) => !req.target || req.target === `local`)
-  const session$ = request$.filter((req) => req.target === `session`)
+import getResponseObj from './util'
 
+export default function(request$) {
   return {
     // For localStorage.
-    local: {
-      key(n) {
-        // Function returning Observable of the nth key.
-        return local$
-          .filter((req) => req.key === localStorage.key(n))
-          .map((req) => req.key)
-          .startWith(localStorage.key(n))
-          .distinctUntilChanged()
-      },
-      // Function returning Observable of item values.
-      getItem(key) {
-        return local$
-          .filter((req) => req.key === key)
-          .map((req) => req.value)
-          .startWith(localStorage.getItem(key))
-      },
+    get local() {
+      return getResponseObj(request$)
     },
     // For sessionStorage.
-    session: {
-      // Function returning Observable of the nth key.
-      key(n) {
-        return session$
-          .filter((req) => req.key === sessionStorage.key(n))
-          .map((req) => req.key)
-          .startWith(sessionStorage.key(n))
-          .distinctUntilChanged()
-      },
-      // Function returning Observable of values.
-      getItem(key) {
-        return session$
-          .filter((req) => req.key === key)
-          .map((req) => req.value)
-          .startWith(sessionStorage.getItem(key))
-      },
+    get session() {
+      return getResponseObj(request$, `session`)
     },
   }
 }
---
+++ src/util.js
@@ -0,0 +1,38 @@
+function getStorage$(request$, type) {
+  if (type === `local`) {
+    return request$.filter((req) => !req.target || req.target === `local`)
+  } else {
+    return request$.filter((req) => req.target === `session`)
+  }
+}
+
+function storageKey(n, request$, type = `local`) {
+  const storage$ = getStorage$(request$, type)
+  const key = type === `local` ?
+    localStorage.key(n) : sessionStorage.key(n)
+
+  return storage$
+    .filter((req) => req.key === key)
+    .map((req) => req.key)
+    .startWith(key)
+    .distinctUntilChanged()
+}
+
+function storageGetItem(key, request$, type = `local`) {
+  const storage$ = getStorage$(request$, type)
+  let storageObj = type === `local` ? localStorage : sessionStorage
+
+  return storage$
+    .filter((req) => req.key === key)
+    .map((req) => req.value)
+    .startWith(storageObj.getItem(key))
+}
+
+export default function getResponseObj(request$, type = `local`) {
+  return {
+    // Function returning Observable of the nth key.
+    key(n) { return storageKey(n, request$, type) },
+    // Function returning Observable of item values.
+    getItem(key) { return storageGetItem(key, request$, type) },
+  }
+}
